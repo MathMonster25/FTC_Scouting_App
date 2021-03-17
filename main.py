2ed4ce1fa34d8de74a8e9751cdd943d6fbcedf09
MathMonster25 <mtt2005mrc2006@gmail.com>
2021-03-17 09:31:53 -0400

    Screen manager with basic screens added

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import os
-os.environ["KIVY_NO_CONSOLELOG"] = "1"
+# When commented out, debug lines get printed
+#os.environ["KIVY_NO_CONSOLELOG"] = "1"
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
+# Ensures compatibility with app if compiled on another computer
 kivy.require("2.0.0")
+
+class HomeScreen(Screen):
+    pass
+
+class SelectTeam(Screen):
+    pass
+
+class AddTeam(Screen):
+    pass
+
+class ScoreSheet(Screen):
+    pass
 
 class MainApp(App):
     def build(self):
-        return Label(text="Hello World!")
+        # Load the main kv file, which contains a screen manager for all kv screens
+        return Builder.load_file("main.kv")
+
+    def change_screen(self, screen_name, direction="left"):
+        # Get the screen manager from the kv file
+        screen_manager = self.root.ids['screen_manager'] # type: ScreenManager
+
+        # Transition using the given direction
+        screen_manager.transition.direction = direction
+        screen_manager.current = screen_name
 
 if __name__ == "__main__":
     chat_app = MainApp()
