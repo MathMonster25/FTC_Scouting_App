45b4e158bfc9adce5cfc87b8661febc72c677dbc
MathMonster25 <mtt2005mrc2006@gmail.com>
2021-03-16 21:30:36 -0400

    Hello World! app screen

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import sys
 import os
+os.environ["KIVY_NO_CONSOLELOG"] = "1"
 import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+kivy.require("2.0.0")
+
+class MainApp(App):
+    def build(self):
+        return Label(text="Hello World!")
 
 if __name__ == "__main__":
-    print("Hello World")
+    chat_app = MainApp()
+    chat_app.run()
